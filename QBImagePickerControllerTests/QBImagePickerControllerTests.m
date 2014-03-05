02b02e6421beec7c15ea95aa61ef5b19498c41c2
Sinoru Dev <sinoru.dev@me.com>
2014-03-05 23:50:40 +0900

    Wrapping as project

---
+++ QBImagePickerControllerTests/QBImagePickerControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  QBImagePickerControllerTests.m
+//  QBImagePickerControllerTests
+//
+//  Created by Sinoru on 2014. 3. 5..
+//  Copyright (c) 2014년 Tanaka Katsuma. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QBImagePickerControllerTests : XCTestCase
+
+@end
+
+@implementation QBImagePickerControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
